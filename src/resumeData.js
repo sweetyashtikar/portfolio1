62fab4a3e0d8bf7bfd96606282cc5125b0927f06
sweety ashtikar <varushaliashtikar@gmail.com>
2023-01-06 15:52:09 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,130 @@
+let resumeData = {
+    "imagebaseurl":"https://sweetyashtikar.github.io/",
+    "name": "Sweety Ashtikar",
+    "role": "MERN stack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like dabbling in various parts of frontend development as well as backend development and like to learn about new technologies, read technical articles or simply read the books in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sweety-ashtikar",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/sweetyashtikar",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/sweetyashtikar",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently working in Kodegurus in chandigarh. I am Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://sweetyashtikar.github.io",
+    "education":[
+      {
+        "UniversityName":"shri ramdeobaba college of engineering and management",
+        "specialization":"Electronic design technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"NIT polytechnic",
+        "specialization":"Electronic telecommunication",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2016",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Kodegurus technology",
+        "specialization":"MERN stack Developer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2022",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Express.js"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"MongoDB"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Kodegurus website",
+        "imgurl":"images/portfolio/kodegurus.png"
+      },
+      {
+        "name":"project2",
+        "description":"E-comm web",
+        "imgurl":"images/portfolio/e-comm web.png"
+      },
+      {
+        "name":"project3",
+        "description":"Monthly Grocery App",  
+        "imgurl":"images/portfolio/monthly app.png"
+      },
+      {
+        "name":"project4",
+        "description":"Restorent app",
+        "imgurl":"images/portfolio/recipe app.png"
+      }
+    ],
+    "testimonials":[
+      {
+        id: 19,
+        name: "Reena",
+        text: "SEO Executive.",
+        rating: 5
+    },
+    {
+        id: 20,
+        name: "Jaspal Singh",
+        text: " Team Leader .",
+        rating: 5
+    },
+    {
+        id: 21,
+        name: "Navdeep Singh ",
+        text: "Web Developer.",
+        rating: 5
+    },
+    {
+        id: 22,
+        name: "Nikhil shishodia ",
+        text: " Business Development Executive & Content Writer.",
+        rating: 5
+    },
+    {
+      id: 23,
+      name: "Sweety Ashtikar ",
+      text: "Web Developer.",
+      rating: 5
+  },
+
+    ]
+  }
+  
+  export default resumeData
